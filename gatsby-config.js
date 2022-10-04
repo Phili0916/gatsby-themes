c215f6ad17fb87a4c7de2af6b41dbb28e14e7d6f
Philip Davis <p.davis@geofit.fr>
2022-10-04 16:18:18 +0200

    added StaticImage Plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,10 @@
 module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
+    title: "First Gatsby Site"
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp"
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 
 import Layout from "../components/layout"
+import { StaticImage } from 'gatsby-plugin-image'
 
 
 
@@ -10,6 +11,10 @@ const IndexPage = () => {
       <Layout pageTitle="Home Page">
         <h1>Welcome to my Gatsby Site</h1>
         <p>This site is dedicated to learning about Gatsby</p>
+        <StaticImage 
+          alt="Wild Thing Major League"
+          src="https://www.washingtonpost.com/blogs/early-lead/files/2016/11/sheen1.jpg"
+        />
       </Layout>
   )
 }
