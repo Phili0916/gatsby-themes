82c2333bf13bef88c4d34c9452eb162bfcbaa912
Philip Davis <p.davis@geofit.fr>
2022-10-05 09:43:48 +0200

    addded blog mdx to blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,15 +1,35 @@
 import * as React from 'react'
+import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
 
-const BlogPage = () => {
+const BlogPage = ({ data }) => {
     return (
         <Layout pageTitle="My Strikeout Posts">
             <p>My fast times posts will go here</p>
+            <ul>
+                {
+                    data.allFile.nodes.map(node => (
+                        <li key={node.name}>
+                            {node.name}
+                        </li>
+                    ))
+                }
+            </ul>
         </Layout>
     )
 }
 
-export const Head = () => <Seo title="My blog Posts" />
+export const query = graphql`
+    query {
+        allFile(filter: {sourceInstanceName: {eq: "blog"}}) {
+            nodes {
+                name
+            }
+        }
+    }
+`
+
+export const Head = () => <Seo title="My blog Posts"  />
 
 export default BlogPage
